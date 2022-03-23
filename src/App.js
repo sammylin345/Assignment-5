4ff29f5eaed22ea6c0dc490373b8fa4bbd6d315d
sammylin345 <43788227+sammylin345@users.noreply.github.com>
2022-03-22 22:49:16 -0500

    added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([])
+
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  useEffect(() => {
+  Axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(res => {
+    console.log(res.data)
+    setData(res.data)
+  }).catch(err => console.log(err))   
+  }, [])
+
+  const postData = (e) => {
+    e.preventDefault();
+    Axios.post('https://jsonplaceholder.typicode.com/posts',{
+      title, body
+    }).then(res => console.log('Posting data',res)).catch(err => console.log(err)) 
+  }
+
+  const postDelete = (id, e) => {
+    e.preventDefault();
+    Axios.delete('https://jsonplaceholder.typicode.com/posts/${id}').
+    then(res => console.log('Deleted data',res)).catch(err => console.log(err)) 
+  }
+
+  const array = data.map((data, index) => {
+      return (
+        <tr>
+          <td style={{border: 'solid black'}}>{data.id}</td>
+          <td style={{border: 'solid black'}}>{data.title}</td>
+          <td style={{border: 'solid black'}}>{data.body}</td>
+          <td style={{border: 'solid black'}}><button onClick={(e) => postDelete(data.id, e)}>Delete</button></td>
+        </tr>
+      )
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Assignment 5: axios with react js</h1>
+      <form>
+        
+        <label>Title</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <hr/>
+        <label>Body</label>
+        <input type="body" value={body} onChange={(e) => setBody(e.target.value)}/>
+        <hr/>
+        <button onClick={postData}>Post</button>
+
+      </form>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+        {array}
+
+      </table>
     </div>
   );
 }
